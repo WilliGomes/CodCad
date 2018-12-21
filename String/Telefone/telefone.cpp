0901a0a8f990e14175369fd5afa9973259a63545
Willigomes <gomes.souza@live.com>
2018-12-21 14:30:52 -0200

    Telefone.cpp feito

--- String/Telefone/telefone.cpp
+++ String/Telefone/telefone.cpp
@@ -2,39 +2,31 @@
 using namespace std;
 
 int main(){
-	string s;
-	cin >> s;
+	string d, n, novo = "";
+	cin >> d >> n;
+		
 
-	for(int i = 0; i < s.size(); i++){
-		if(s[i] == 'A' or s[i] == 'B' or s[i] == 'C'){
-			cout << "2";
-		}
-		else if(s[i] == 'D' or s[i] == 'E' or s[i] == 'F' ){
-			cout << "3";
-		}
-		else if( s[i] == 'G' or s[i] == 'H' or s[i] == 'I'){
-			cout << "4";
-		}
-		else if( s[i] == 'J' or s[i] == 'K' or s[i] == 'L'){
-			cout << "5";
+	while(d != "0" and n != "0"){
+		for(int i = 0; i < n.size(); i++){
+			if(n[i] != d[0]){
+				novo = novo + n[i];
+			}
 		}	
-		else if( s[i] == 'M' or s[i] == 'N' or s[i] == 'O'){
-			cout << "6";
-		}	
-		else if( s[i] == 'P' or s[i] == 'Q' or s[i] == 'R' or s[i] == 'S'){
-			cout << "7";
-		}	
-		else if( s[i] == 'T' or s[i] == 'U' or s[i] == 'V'){
-			cout << "8";
-		}	
-		else if(s[i] == 'W' or s[i] == 'X' or s[i] == 'Y' or s[i] == 'Z' ){
-			cout << "9";
-		}
-		else{
-			cout << s[i];	
+
+		int verificador = 0;
+		for(int i = 0; i < novo.size(); i++){
+			if(novo[i] == '0')
+				verificador = 1;
+	
 		}
 
+		if(novo == "" || verificador == 1)
+			novo = "0";
+
+		cout << novo << endl;
+		
+		novo = "";
+		cin >> d >> n;
 	}
-
 
 }
